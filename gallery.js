189074346ab791111eccba4b2c4266d18e9bc5a8
Stefan Penner <stefan.penner@gmail.com>
2010-11-04 21:30:42 -0500

    cleanup, splitup lib form css.

---
+++ gallery.js
@@ -0,0 +1,55 @@
+var Site = Site || {};
+
+
+// Gallery Plugin
+Site.gallery = function(options){
+  $.extend(true,this,options);
+};
+
+Site.gallery.prototype = { 
+  viewer:     null,
+  scrollable: null,
+  
+  onSeek: function(a,index){
+    this.getItems().
+      eq(index).
+      find('img:first').
+      trigger('click');
+  },
+
+  init: function(){
+    var scrollable = this.scrollable,
+        viewer     = this.viewer;
+
+    scrollable.
+      scrollable({ onSeek: this.scrollableOptions }).
+      find('.item img').
+        live('click',function(){
+          var element = $(this),
+            newSrc = element.attr('data-src'),
+            oldImage = viewer.find('img'),
+            newImage = $(new Image());
+          
+          scrollable.
+            find('img').
+            removeClass('current');
+
+          element.addClass('current');
+          
+          oldImage.before(newImage);
+          newImage.load(function(){ 
+            oldImage.fadeOut('fast',function(){
+              oldImage.remove();
+            });
+          }).attr('src',newSrc);
+        });
+  },
+
+  run: function(){
+  //fake human mouse click to start it off.
+    this.scrollable.
+      find('.item img').
+      first().
+      trigger('click');
+  }
+};
