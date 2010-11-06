4d957f87fdcdcff2ecd31e69e0805b857b10a71f
Stefan Penner <stefan.penner@gmail.com>
2010-11-06 15:08:20 -0500

    Closurize.

--- gallery.js
+++ gallery.js
@@ -1,55 +1,59 @@
 var Site = Site || {};
 
 
-// Gallery Plugin
-Site.gallery = function(options){
-  $.extend(true,this,options);
-};
+(function(Site){
 
-Site.gallery.prototype = { 
-  viewer:     null,
-  scrollable: null,
+  // Gallery Plugin
+  Site.gallery = function(options){
+    $.extend(true,this,options);
+  };
   
-  onSeek: function(a,index){
-    this.getItems().
-      eq(index).
-      find('img:first').
-      trigger('click');
-  },
-
-  init: function(){
-    var scrollable = this.scrollable,
-        viewer     = this.viewer;
-
-    scrollable.
-      scrollable({ onSeek: this.scrollableOptions }).
-      find('.item img').
-        live('click',function(){
-          var element = $(this),
-            newSrc = element.attr('data-src'),
-            oldImage = viewer.find('img'),
-            newImage = $(new Image());
-          
-          scrollable.
-            find('img').
-            removeClass('current');
-
-          element.addClass('current');
-          
-          oldImage.before(newImage);
-          newImage.load(function(){ 
-            oldImage.fadeOut('fast',function(){
-              oldImage.remove();
-            });
-          }).attr('src',newSrc);
-        });
-  },
-
-  run: function(){
-  //fake human mouse click to start it off.
-    this.scrollable.
-      find('.item img').
-      first().
-      trigger('click');
-  }
-};
+  Site.gallery.prototype = { 
+    viewer:     null,
+    scrollable: null,
+    
+    onSeek: function(a,index){
+      this.getItems().
+        eq(index).
+        find('img:first').
+        trigger('click');
+    },
+  
+    init: function(){
+      var scrollable = this.scrollable,
+          viewer     = this.viewer;
+  
+      scrollable.
+        scrollable({ onSeek: this.scrollableOptions }).
+        find('.item img').
+          live('click',function(){
+            var element = $(this),
+              newSrc = element.attr('data-src'),
+              oldImage = viewer.find('img'),
+              newImage = $(new Image());
+            
+            scrollable.
+              find('img').
+              removeClass('current');
+  
+            element.addClass('current');
+            
+            oldImage.before(newImage);
+            newImage.load(function(){ 
+              oldImage.fadeOut('fast',function(){
+                console.log("remove");
+                oldImage.remove();
+              });
+            }).attr('src',newSrc);
+          });
+    },
+  
+    run: function(){
+    //fake human mouse click to start it off.
+      this.scrollable.
+        find('.item img').
+        first().
+        trigger('click');
+    }
+  };
+})(Site);
