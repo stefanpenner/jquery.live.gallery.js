ec5fcea48e493c2a90167955fdc9cc55bbf546f7
Stefan Penner <stefan.penner@gmail.com>
2011-07-04 16:17:21 -0500

    begin update

--- gallery.js
+++ gallery.js
@@ -1,58 +1,87 @@
-var Site = Site || {};
+(function(){
+  $.fn.imageFadeIn = function(){
+    this.each(function(){
+      var image = $(this),
+            src = image.attr('src');
 
-(function(Site){
-
-  // Gallery Plugin
-  Site.gallery = function(options){
-    $.extend(true,this,options);
+      image.load(function(){
+        image.fadeIn();
+      }).attr('src',src);
+    });
   };
-  
-  Site.gallery.prototype = { 
+
+  var Gallery = $.fn.liveGallery = function(options){
+    $.extend(true,this,options);
+  },
+
+  // set your own global custom prefix
+  PREFIX      = $.fn.PREFIX      = $.fn.PREFIX      || '.';
+
+  Gallery.prototype = {
     viewer:     null,
     scrollable: null,
-    
+    gallerySelector:    PREFIX+'gallery',
+    viewerSelector:     PREFIX+'viewer',
+    currentSelector:    PREFIX+'current',
+    scrollableSelector: PREFIX+'scrollable',
+    thumbnailSelector:  PREFIX+'item img',
+
     onSeek: function(a,index){
       this.getItems().
         eq(index).
         find('img:first').
         trigger('click');
     },
-  
+
     init: function(){
       var scrollable = this.scrollable,
           viewer     = this.viewer;
-  
+
       scrollable.
-        scrollable({ onSeek: this.scrollableOptions }).
-        find('.item img').
+        scrollable({ onSeek: this.onSeek }).
+        find(this.thumbnailSelector).
           live('click',function(){
             var element = $(this),
               newSrc = element.attr('data-src'),
               oldImage = viewer.find('img'),
               newImage = $(new Image());
-            
-            scrollable.
+
+              scrollable.
               find('img').
-              removeClass('current');
-  
-            element.addClass('current');
-            
+              removeClass(this.currentSelector);
+
+            element.addClass(this.currentSelector);
+
             oldImage.before(newImage);
-            newImage.load(function(){ 
+            newImage.load(function(){
               oldImage.fadeOut('fast',function(){
-                console.log("remove");
                 oldImage.remove();
               });
             }).attr('src',newSrc);
           });
     },
-  
+
     run: function(){
     //fake human mouse click to start it off.
       this.scrollable.
-        find('.item img').
+        find(this.thumbnailSelector).
         first().
         trigger('click');
     }
   };
-})(Site);
+
+  Gallery.run = function(){
+    $('img.'+PREFIX+'fade-in').imageFadeIn();
+    var gallery = new Site.gallery({
+        viewer:     $(this.viewerSelector),
+        scrollable: $(this.scrollableSelector)
+    });
+
+    gallery.init();
+    gallery.run();
+  };
+
+  $(function(){
+    $(this.gallerySelector).liveGallery();
+  });
+})();
